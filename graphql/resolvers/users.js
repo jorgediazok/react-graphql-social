9794981bf826bbec48deaab03c782653d4f19045
Jorge <jorgediazok@gmail.com>
2020-11-11 18:09:26 -0300

    login and register working

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,20 +1,59 @@
-// @ts-nocheck
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { UserInputError } = require('apollo-server');
 require('dotenv').config();
 
-const { validateRegisterInput } = require('../../util/validators');
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require('../../util/validators');
 const User = require('../../models/User');
+
+function generateToken(user) {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    process.env.SECRET_KEY,
+    { expiresIn: '1h' }
+  );
+}
 
 module.exports = {
   Mutation: {
+    async login(_, { username, password }) {
+      const { errors, valid } = validateLoginInput(username, password);
+
+      if (!valid) {
+        throw new UserInputError('Errors', { errors });
+      }
+
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        errors.general = 'User not found';
+        throw new UserInputError('User not found', { errors });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        errors.general = 'Wrong credentials';
+        throw new UserInputError('Wrong credentials', { errors });
+      }
+
+      const token = generateToken(user);
+
+      return {
+        ...user._doc,
+        id: user._id,
+        token,
+      };
+    },
+
     async register(
       _,
-      { registerInput: { username, email, password, confirmPassword } },
-      args,
-      context,
-      info
+      { registerInput: { username, email, password, confirmPassword } }
     ) {
       //Valudate user data
       const { valid, errors } = validateRegisterInput(
@@ -62,15 +101,8 @@ module.exports = {
 
       const result = await newUser.save();
 
-      const token = jwt.sign(
-        {
-          id: result.id,
-          email: result.email,
-          username: result.username,
-        },
-        process.env.SECRET_KEY,
-        { expiresIn: '1h' }
-      );
+      const token = generateToken(result);
+
       return {
         ...result._doc,
         id: result._id,
