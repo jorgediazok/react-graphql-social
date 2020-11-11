a7e0d264fb443ab3980cb5806f3f3a5b7d565320
Jorge <jorgediazok@gmail.com>
2020-11-11 03:21:42 -0300

    graphql connected to collection

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
-
 require('dotenv').config();
 
+const Post = require('./models/Post');
+const User = require('./models/User');
+
+//Graphql
 // @ts-ignore
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
+
   type Query {
-    sayHi: String!
+    getPosts: [Post]
   }
 `;
 
 const resolvers = {
   Query: {
-    sayHi: () => 'Hello World',
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
   },
 };
 
