68a31d6d908e56d8d964940b9a64c36df8053778
Jorge <jorgediazok@gmail.com>
2020-11-11 02:47:37 -0300

    connected to mongoose

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 // @ts-ignore
 const typeDefs = gql`
@@ -18,6 +21,18 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
+
+//Connection to DB
+mongoose
+  .connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((db) => console.log('DB is connected!!'))
+  .catch((err) => console.log(err));
+
+//Server
 
 server.listen({ port: 5000 }).then((res) => {
   console.log(`Server running at ${res.url}`);
