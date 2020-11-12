481fb06f9d8c54e932aeb9501960cb107c58d33e
Jorge <jorgediazok@gmail.com>
2020-11-12 14:05:50 -0300

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import MenuBar from './components/MenuBar';
+
+//CSS
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    <h1>This is my app</h1>
-    </div>
+    <Router>
+      <MenuBar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </Router>
   );
 }
 
